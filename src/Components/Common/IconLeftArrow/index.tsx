bc349d1208f93975cb5d3ba2712bd8d4e02a48a1
Josh Clyde <joshclyde11@gmail.com>
2022-06-20 13:10:27 -0400

    Revise arrow icons to not use the transform property because it was causing issues on iphone

--- src/Components/Common/IconLeftArrow/index.tsx
+++ src/Components/Common/IconLeftArrow/index.tsx
@@ -13,82 +13,17 @@ export const IconLeftArrow: FC<any> = (props) => {
       {...props}
       width="21"
       height="16"
-      rotate="180"
       viewBox="0 0 21 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect
-        x="6"
-        y="16"
-        width="16"
-        height="3"
-        transform="rotate(-90 6 16)"
-        fill={COLORS.YELLOW}
-      />
-      <rect
-        x="4"
-        y="14"
-        width="12"
-        height="4"
-        transform="rotate(-90 4 14)"
-        fill={COLORS.YELLOW}
-      />
-      <rect
-        x="2"
-        y="12"
-        width="8"
-        height="11"
-        transform="rotate(-90 2 12)"
-        fill={COLORS.YELLOW}
-      />
-      <rect
-        y="10"
-        width="4"
-        height="12"
-        transform="rotate(-90 0 10)"
-        fill={COLORS.YELLOW}
-      />
-      <rect
-        x="8"
-        y="13"
-        width="10"
-        height="5"
-        transform="rotate(-90 8 13)"
-        fill={COLORS.BLACK}
-      />
-      <rect
-        x="6"
-        y="11"
-        width="4"
-        height="5"
-        transform="rotate(-90 6 11)"
-        fill={COLORS.BLACK}
-      />
-      <rect
-        x="6"
-        y="9"
-        width="4"
-        height="5"
-        transform="rotate(-90 6 9)"
-        fill={COLORS.BLACK}
-      />
-      <rect
-        x="4"
-        y="9"
-        width="2"
-        height="5"
-        transform="rotate(-90 4 9)"
-        fill={COLORS.BLACK}
-      />
-      <rect
-        x="4"
-        y="10"
-        width="4"
-        height="17"
-        transform="rotate(-90 4 10)"
-        fill={COLORS.YELLOW}
-      />
+      <rect y="6" width="21" height="4" fill={COLORS.YELLOW} />
+      <rect x="2" y="4" width="4" height="2" fill={COLORS.YELLOW} />
+      <rect x="2" y="10" width="4" height="2" fill={COLORS.YELLOW} />
+      <rect x="4" y="2" width="4" height="3" fill={COLORS.YELLOW} />
+      <rect x="4" y="11" width="4" height="3" fill={COLORS.YELLOW} />
+      <rect x="6" width="3" height="3" fill={COLORS.YELLOW} />
+      <rect x="6" y="13" width="3" height="3" fill={COLORS.YELLOW} />
     </Svg>
   );
 };
--- src/Components/Common/IconRightArrow/index.tsx
+++ src/Components/Common/IconRightArrow/index.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 
-import { IconLeftArrow } from "../IconLeftArrow";
+import { COLORS } from "@Utils";
 
-export const IconRightArrow: FC<any> = (props) => (
-  <IconLeftArrow transform="rotate(180)" {...props} />
-);
+const Svg = styled.svg`
+  cursor: pointer;
+`;
+
+export const IconRightArrow: FC<any> = (props) => {
+  return (
+    <Svg
+      {...props}
+      width="21"
+      height="16"
+      viewBox="0 0 21 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect y="6" width="21" height="4" fill={COLORS.YELLOW} />
+      <rect x="15" y="4" width="4" height="2" fill={COLORS.YELLOW} />
+      <rect x="15" y="10" width="4" height="2" fill={COLORS.YELLOW} />
+      <rect x="13" y="2" width="4" height="3" fill={COLORS.YELLOW} />
+      <rect x="13" y="11" width="4" height="3" fill={COLORS.YELLOW} />
+      <rect x="12" width="3" height="3" fill={COLORS.YELLOW} />
+      <rect x="12" y="13" width="3" height="3" fill={COLORS.YELLOW} />
+    </Svg>
+  );
+};
